588a07cd04ea9c4b699915acd8137dc205f644f5
devdevil-production <saudd2gh@gmail.com>
2020-04-17 23:00:31 +0400

    Minor changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import React from "react";
 import "animate.css/animate.min.css";
 import "../styles/main.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
@@ -11,9 +12,7 @@ export default function Layout({ children }) {
       <main>
         <div className="container">{children}</div>
       </main>
-      <footer>
-        <div>This is a footer</div>
-      </footer>
+      <Footer />
     </>
   );
 }
--- static/projects/canella-restaurant.md
+++ static/projects/canella-restaurant.md
@@ -1,5 +1,5 @@
 ---
-title: Canell Restaurant
+title: Canella Restaurant
 date: 2020-03-24
 img: ../images/canella-restaurant-hero.png
 url: https://devdevil94.github.io/Canella-Restaurant
