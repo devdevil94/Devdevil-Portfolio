39d56318a0f72242fb80226efb1ffd746f6fce81
devdevil-production <saudd2gh@gmail.com>
2020-04-17 14:16:08 +0400

    Add scroll animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
+import ScrollAnimation from "react-animate-on-scroll";
 import Layout from "./../components/Layout";
 
 const IndexPage = ({ data }) => {
@@ -8,45 +9,50 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       {(edges || []).map(({ node: { id, frontmatter: project } }, i) => (
-        <div
+        <ScrollAnimation
           key={id}
-          className={`projectContainer${
-            i % 2 === 0 ? "" : " projectContainer--reverse"
-          }`}
+          animateOnce={true}
+          animateIn={i % 2 === 0 ? "fadeInRight" : "fadeInLeft"}
         >
-          <Img
-            fluid={project.img.childImageSharp.fluid}
-            className="float-md projectContainer__img"
-          />
-          <div className="projectContainer__info">
-            <h2 className="text-dark projectContainer__title">
-              {project.title}
-            </h2>
-            <p className="text-grey projectContainer__description">
-              {project.description}
-            </p>
-            <h3 className="text-dark projectContainer__techListHeader">
-              Technologies
-            </h3>
-            <ul className="projectContainer__techList">
-              {(project.tech || []).map((tech, i) => (
-                <li
-                  key={i}
-                  className="text-secondary bg-white rounded projectContainer__techItem"
-                >
-                  {tech}
-                </li>
-              ))}
-            </ul>
-            <a
-              href={project.url}
-              target="_blank"
-              className="bg-primary text-white uppercase rounded projectContainer__visitBtn"
-            >
-              Visit Website
-            </a>
+          <div
+            className={`projectContainer${
+              i % 2 === 0 ? "" : " projectContainer--reverse"
+            }`}
+          >
+            <Img
+              fluid={project.img.childImageSharp.fluid}
+              className="float-md projectContainer__img"
+            />
+            <div className="projectContainer__info">
+              <h2 className="text-dark projectContainer__title">
+                {project.title}
+              </h2>
+              <p className="text-grey projectContainer__description">
+                {project.description}
+              </p>
+              <h3 className="text-dark projectContainer__techListHeader">
+                Technologies
+              </h3>
+              <ul className="projectContainer__techList">
+                {(project.tech || []).map((tech, i) => (
+                  <li
+                    key={i}
+                    className="text-secondary bg-white rounded projectContainer__techItem"
+                  >
+                    {tech}
+                  </li>
+                ))}
+              </ul>
+              <a
+                href={project.url}
+                target="_blank"
+                className="bg-primary text-white uppercase rounded projectContainer__visitBtn"
+              >
+                Visit Website
+              </a>
+            </div>
           </div>
-        </div>
+        </ScrollAnimation>
       ))}
     </Layout>
   );
