345a513466effef6bed5398ee649be4cf8008a72
devdevil-production <saudd2gh@gmail.com>
2020-04-18 11:20:37 +0400

    Add contact form & image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import ContactForm from "./ContactForm";
+import ContactFormImg from "./ContactFormImg";
 
 export default function Footer() {
   return (
-    <footer>
-      <p>&copy; 2020 Devdevil, All Rights Reserved</p>
+    <footer className="footer">
+      <div className="footer__column">
+        <ContactFormImg />
+      </div>
+      <div className="footer__column">
+        <ContactForm />
+      </div>
     </footer>
   );
 }
