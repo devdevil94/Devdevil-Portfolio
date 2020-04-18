71b8082feb2d1f1eea6cc4e88cac9949569e4f1a
devdevil-production <saudd2gh@gmail.com>
2020-04-18 11:19:26 +0400

    Add contact form inputs

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,8 +2,50 @@ import React from "react";
 
 export default function ContactForm() {
   return (
-    <div>
-      <h1>Contact Form</h1>
-    </div>
+    <form className="contactForm validate-form">
+      <span className="contactForm__title">Get in touch</span>
+
+      <div className="field validate-input" data-validate="Name is required">
+        <input
+          className="field__input"
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+        <span className="field__inputFocus"></span>
+        <span className="field__inputIcon">
+          <i className="fa fa-user" aria-hidden="true"></i>
+        </span>
+      </div>
+
+      <div
+        className="field validate-input"
+        data-validate="Valid email is required: ex@abc.xyz"
+      >
+        <input
+          className="field__input"
+          type="text"
+          name="email"
+          placeholder="Email"
+        />
+        <span className="field__inputFocus"></span>
+        <span className="field__inputIcon">
+          <i className="fa fa-envelope" aria-hidden="true"></i>
+        </span>
+      </div>
+
+      <div className="field validate-input" data-validate="Message is required">
+        <textarea
+          className="field__input"
+          name="message"
+          placeholder="Message"
+        ></textarea>
+        <span className="field__inputFocus"></span>
+      </div>
+
+      <div className="contactForm__btnContainer">
+        <button className="contactForm__btn">Send</button>
+      </div>
+    </form>
   );
 }
