b0153a65d2f8488c3aba90af9ef1ec5e4e0abbf2
devdevil-production <saudd2gh@gmail.com>
2020-04-19 19:01:01 +0400

    Add scroll animation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,21 @@
 import React from "react";
 import ContactForm from "./ContactForm";
 import ContactFormImg from "./ContactFormImg";
+import ScrollAnimation from "react-animate-on-scroll";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="footer__column footer__column--left">
-          <ContactFormImg />
+      <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+        <div className="container">
+          <div className="footer__column footer__column--left">
+            <ContactFormImg />
+          </div>
+          <div className="footer__column footer__column--right">
+            <ContactForm />
+          </div>
         </div>
-        <div className="footer__column footer__column--right">
-          <ContactForm />
-        </div>
-      </div>
+      </ScrollAnimation>
     </footer>
   );
 }
