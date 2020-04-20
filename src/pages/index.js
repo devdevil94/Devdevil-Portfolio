de1d9bffac2f8f0de7784dc8007dd4c64b9a2a7e
devdevil-production <saudd2gh@gmail.com>
2020-04-20 12:41:45 +0400

    Minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,53 +8,67 @@ const IndexPage = ({ data }) => {
   const edges = data.allMarkdownRemark.edges;
   return (
     <Layout>
-      {(edges || []).map(({ node: { id, frontmatter: project } }, i) => (
-        <ScrollAnimation
-          key={id}
-          animateOnce={true}
-          // duration={0.7}
-          animateIn={i % 2 === 0 ? "fadeInRight" : "fadeInLeft"}
-        >
-          <div
-            className={`projectContainer${
-              i % 2 === 0 ? "" : " projectContainer--reverse"
-            }`}
+      <section className="projectsSection">
+        <h2 className="text-dark projectsSection__header">My Projects</h2>
+        {(edges || []).map(({ node: { id, frontmatter: project } }, i) => (
+          <ScrollAnimation
+            key={id}
+            animateOnce={true}
+            // duration={0.7}
+            animateIn={i % 2 === 0 ? "fadeInRight" : "fadeInLeft"}
           >
-            <Img
-              fluid={project.img.childImageSharp.fluid}
-              className="float-md projectContainer__img"
-            />
-            <div className="projectContainer__info">
-              <h2 className="text-dark projectContainer__title">
-                {project.title}
-              </h2>
-              <p className="text-grey projectContainer__description">
-                {project.description}
-              </p>
-              {/* <h3 className="text-dark projectContainer__techListHeader">
+            <div
+              className={`projectContainer${
+                i % 2 === 0 ? "" : " projectContainer--reverse"
+              }`}
+            >
+              <Img
+                fluid={project.img.childImageSharp.fluid}
+                className="float-md projectContainer__img"
+              />
+              <div className="projectContainer__info">
+                <h3 className="text-dark projectContainer__title">
+                  {project.title}
+                </h3>
+                <p className="text-grey projectContainer__description">
+                  {project.description}
+                </p>
+                {/* <h3 className="text-dark projectContainer__techListHeader">
                 Technologies
               </h3> */}
-              <ul className="projectContainer__techList">
-                {(project.tech || []).map((tech, i) => (
-                  <li
-                    key={i}
-                    className="text-primary bg-white rounded projectContainer__techItem"
+                <ul className="projectContainer__techList">
+                  {(project.tech || []).map((tech, i) => (
+                    <li
+                      key={i}
+                      className="text-dark projectContainer__techItem"
+                    >
+                      {tech}
+                    </li>
+                  ))}
+                </ul>
+                <div>
+                  <a
+                    href={project.url}
+                    target="_blank"
+                    className="text-white uppercase rounded projectContainer__websiteBtn"
                   >
-                    {tech}
-                  </li>
-                ))}
-              </ul>
-              <a
-                href={project.url}
-                target="_blank"
-                className="text-white uppercase rounded projectContainer__visitBtn"
-              >
-                Visit Website
-              </a>
+                    Website
+                  </a>
+                  {project.repo && (
+                    <a
+                      href={project.repo}
+                      target="_blank"
+                      className="text-white uppercase rounded projectContainer__codeBtn"
+                    >
+                      Code
+                    </a>
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
-        </ScrollAnimation>
-      ))}
+          </ScrollAnimation>
+        ))}
+      </section>
     </Layout>
   );
 };
@@ -71,6 +85,7 @@ export const projectsQuery = graphql`
             description
             tech
             url
+            repo
             img {
               childImageSharp {
                 fluid(maxWidth: 1000, quality: 90) {
